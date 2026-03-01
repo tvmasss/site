cfe9ccc8d7f03e9b8cbd93537ee5ed6c74dd441d
tvmasss <xbvcbz@gmail.com>
2026-03-02 00:18:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const SITE_CONFIG = {
     
     // Ссылки для поиска
     searchPlaceholder: "Найти...",
-    apiSearchUrl: "https://bitmom.ru/handler.php"
+    apiSearchUrl: "https://wikido.online/handler.php"
 };
 
 // Функция для автоматического заполнения общих элементов (шапка, футер)
@@ -40,4 +40,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+
 
